6a7eb9f55dbbecbf63b2d1204ef91452ee4bd9a4
InsomniacGuy <rahulchandan1991@gmail.com>
2024-04-21 10:56:41 +0530

    Add links, selected language parameter en, added limit to number of articles, control category with fstring.

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ import requests
 from send_email import send_email
 
 api_key = "1482cdf6c9df493abb682d6980cf1816"
+topic = "tesla"
 
 # creating a special request object. Make request
-request = requests.get("https://newsapi.org/v2/everything?q=tesla&from=2024-03-19&sortBy=publishedAt&apiKey"
-                       "=1482cdf6c9df493abb682d6980cf1816")
+request = requests.get("https://newsapi.org/v2/everything?"
+                       f"q={topic}&"
+                       "from=2024-04-20&"
+                       "sortBy=publishedAt&apiKey"
+                       "=1482cdf6c9df493abb682d6980cf1816&"
+                       "language=en")
 
 # Get dictionary with data
 content = request.json()
 print(content)
 
 news = ""
-for article in content['articles']:
-    news = news + str(article['title']) + "\n" + str(article['description'])
+for article in content['articles'][0:10]:
+    if article['title'] is not None and article['description'] is not None:
+        news = (str(news) + str(article['title']) + str("\n")
+                + str(article['description'] + str(2 * "\n")) + "\n" + article['url'])
 
 print(news)
 
 message = f"""\
-Subject: News titles and descriptions on Tesla
+Subject: Today's news
 
 From: r00t7h3ll@gmail.com
 {news}
